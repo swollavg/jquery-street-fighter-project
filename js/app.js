3e6e1570591c032196237f397667f129f1146378
Antony Grove <agrove86@gmail.com>
2014-06-18 21:35:03 -0500

    add sound

--- js/app.js
+++ js/app.js
@@ -11,11 +11,20 @@ $(document).ready(function(){
 	})
 
 	.mousedown(function(){
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-        // show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show()
+
+		.animate(
+			{'left': '300px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
+
 	})
 
 	.mouseup(function() {
@@ -24,3 +33,10 @@ $(document).ready(function(){
 	});
 
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+
+}
